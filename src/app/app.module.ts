9d35ca738ad65f989798686dc8f415c59a587c7c
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2020-09-22 19:56:35 -0400

    angular 10 pipe example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
+import { AppComponent } from './components/app/app.component';
+import { ChildComponent } from './components/child/child.component';
 
-import { AppComponent } from './app.component';
+@Pipe({name: 'round'})
+export class RoundPipe implements PipeTransform {
+  transform(value: number): number {
+    return Math.round(value);
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, RoundPipe, ChildComponent
   ],
   imports: [
     BrowserModule
